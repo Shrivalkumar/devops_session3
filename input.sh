9517afc27425f4c0f7c972e3e8e4c0e1bdb4ed82
shrivalkumar <shrivalcpp@gmail.com>
2026-08-18 16:56:43 +0530

    input

---
+++ input.sh
@@ -0,0 +1,7 @@
+read -p "Name enter maadi: " name
+read -p "rollno enter maadi: " rollno
+read -p "comment enter maadi: " comment
+
+echo "your name is $name"
+echo "your rollno is $rollno"
+echo $comment
