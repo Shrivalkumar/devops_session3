a9887e21d4faef8151650c35c82fc48effe0ea7b
shrivalkumar <shrivalcpp@gmail.com>
2026-08-18 16:42:33 +0530

    variable

---
+++ data.sh
@@ -0,0 +1,9 @@
+mkdir data1
+cd data1
+touch app.log
+echo "this is a log file" > app.log
+cat app.log
+
+echo "this is file 1" > app.log
+cat app.log
+
---
+++ variable.sh
@@ -0,0 +1,7 @@
+name="Shrival Kumar"
+rollno="24bcs10254"
+comment="cwazy"
+
+echo "my name is $name"
+echo "my rollno is $rollno"
+echo "and I am $comment"
